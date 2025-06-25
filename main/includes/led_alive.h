6f140203cd227721a0c204dd609401e376b37851
MinoFloof <bha9465@students.fhv.at>
2025-06-25 20:53:12 +0200

    ToDo as reminder

--- main/includes/led_alive.h
+++ main/includes/led_alive.h
@@ -2,5 +2,7 @@
 
 #include "esp_err.h"
 
-void led_program_alive_task(void *pvParameters);
+void led_program_alive_task_start(void *pvParameters);
+// ToDo: LED Task Error (red blinking)
+
 esp_err_t led_init();
